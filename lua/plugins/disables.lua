11ae9e1636cf37fa02bacbb6c7feaa599304ec32
killkli <dchensterebay@gmail.com>
2024-01-14 12:26:29 +0800

    Initial Commit

---
+++ lua/plugins/disables.lua
@@ -0,0 +1,4 @@
+return {
+  -- disable trouble
+  { "dressing.nvim", enabled = false },
+}
---
+++ lua/plugins/myplugins.lua
@@ -0,0 +1,47 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      style = "night",
+    },
+  },
+  -- change trouble config
+  {
+    "folke/trouble.nvim",
+    -- opts will be merged with the parent spec
+    opts = { use_diagnostic_signs = true },
+  },
+
+  -- add symbols-outline
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    config = true,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "vim",
+        "yaml",
+        "rust",
+        "go",
+        "vue",
+      })
+    end,
+  },
+}
